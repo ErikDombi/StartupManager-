bb44758f051da625d909fe476e687dbaa0f86307
wickedlizerd <erikd02@gmail.com>
2017-11-12 22:44:15 -0500

    Add files via upload

---
+++ StartupManager+/StartupManager+/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace StartupManager_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Font = new System.Drawing.Font("Gotham Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "StartupManager+";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox2.Location = new System.Drawing.Point(738, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 16);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 399);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(786, 25);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(783, 423);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ StartupManager+/StartupManager+/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StartupManager_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        public void refresh()
+        {
+            this.Controls.Clear();
+
+            PictureBox exitButton = new PictureBox() { Name = "pictureBox2", Size = new Size(48, 16), Location = new Point(this.Width - 48, 0), BackColor = Color.FromArgb(64, 64, 64) };
+            exitButton.Click += (s, e) => { Environment.Exit(0); }; exitButton.MouseEnter += (s, e) => { exitButton.BackColor = Color.FromArgb(30, 30, 30); }; exitButton.MouseLeave += (s, e) => { exitButton.BackColor = Color.FromArgb(64, 64, 64); };
+            this.Controls.Add(exitButton);
+
+            Button addProgram = new Button() { Name = "AddProgram", Text = "Add new program", Size = new Size(this.Width + 4, 30), Location = new Point(-2, this.Height - 28), FlatStyle = FlatStyle.Flat, BackColor = Color.White, ForeColor = Color.Black };
+            addProgram.Click += (s, e) =>
+            {
+                openFileDialog1.FileName = "";
+                openFileDialog1.Title = "Select file to add to startup";
+                openFileDialog1.ShowDialog();
+                try
+                {
+                    if (!File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), openFileDialog1.SafeFileName))) File.Copy(openFileDialog1.FileName, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), openFileDialog1.SafeFileName));
+                    else
+                    {
+                        File.WriteAllBytes(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), openFileDialog1.SafeFileName), File.ReadAllBytes(openFileDialog1.FileName));
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("An error occured why trying to add the program the startup. Please try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                refresh();
+            };
+            this.Controls.Add(addProgram);
+
+            Label title = new Label() { Name = "label1", Location = new Point(10, 7), Size = new Size(222, 28), BackColor = Color.DodgerBlue, Text = "StartupManager+", ForeColor = Color.White, Font = new System.Drawing.Font("Gotham Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0))) };
+            title.MouseDown += (s, e) =>
+            {
+                formO = this.Location;
+                mouseO = MousePosition;
+                timer1.Start();
+            };
+            title.MouseUp += (s, e) =>
+            {
+                timer1.Stop();
+                if (mouseO == MousePosition) refresh();
+            };
+            this.Controls.Add(title);
+
+            PictureBox topBar = new PictureBox() { Name = "pictureBox1", Location = new Point(0, 0), Size = new Size(this.Width, 43), BackColor = Color.DodgerBlue };
+            topBar.MouseDown += (s, e) =>
+            {
+                formO = this.Location;
+                mouseO = MousePosition;
+                timer1.Start();
+            };
+            topBar.MouseUp += (s, e) =>
+            {
+                timer1.Stop();
+            };
+            this.Controls.Add(topBar);
+
+            int count = 0;
+            var files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Startup)).Where(x => new FileInfo(x).Length != 0 && !x.ToLower().Contains("desktop.ini")).ToList();
+            if(files.Count > 0) foreach (var file in files)
+                {
+                    Label name = new Label();
+                    name.AutoSize = true;
+                    name.Font = new Font("Gotham Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    name.Location = new Point(10, 43 + 10 + count * 40);
+                    name.Size = new Size(100, 200);
+                    name.TabIndex = 1;
+                    name.Text = file.Split('\\').LastOrDefault();
+                    name.Name = $"application{count}";
+                    name.ForeColor = Color.Black;
+                    name.BackColor = Color.White;
+                    this.Controls.Add(name);
+
+                    PictureBox splitter = new PictureBox();
+                    splitter.Name = $"splitter{count}";
+                    splitter.BackColor = Color.Gray;
+                    splitter.Location = new Point(0, 43 + 37 + count * 40);
+                    splitter.Size = new Size(this.Width, 1);
+                    this.Controls.Add(splitter);
+
+                    Button remove = new Button();
+                    remove.Name = $"remove{count}";
+                    remove.Text = "Remove";
+                    remove.Location = new Point(this.Width - 70, 43 + 6 + count * 40);
+                    remove.Click += (s, e) => { File.WriteAllBytes(file, new byte[0]); refresh(); };
+                    remove.Size = new Size(60, 25);
+                    this.Controls.Add(remove);
+
+                    count++;
+                }
+            else
+            {
+                Label noPrograms = new Label() { Name = "NoProgramWarning", Text = "No programs yet :(\nYou can add some by pressing the button below", AutoSize = false, TextAlign = ContentAlignment.MiddleCenter, Dock = DockStyle.Fill, Font = new Font("Gotham Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))), ForeColor = Color.Gray, Location = new Point(0, 0) };
+                this.Controls.Add(noPrograms);
+            }
+        }
+
+
+        Point formO;
+        Point mouseO;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            formO = this.Location;
+            mouseO = MousePosition;
+            timer1.Start();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Left = MousePosition.X - (mouseO.X - formO.X);
+            this.Top = MousePosition.Y - (mouseO.Y - formO.Y);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+    }
+}
